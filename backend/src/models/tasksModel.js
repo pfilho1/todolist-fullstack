f9b23df083a8e64eac37714435abb3b5ef4a4be8
Your Name <you@example.com>
2023-08-10 10:41:37 -0300

    codes

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,7 @@
+const getAll = () => {       //getAll retorna todas as tasks presentes no Banco de Dados
+
+};
+
+module.exports = {
+    getAll
+};
---
+++ backend/src/router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+// router.get('/tasks', (req, res) => res.status(200).send('O router esta funcionando!'));
+
+module.exports = router;
